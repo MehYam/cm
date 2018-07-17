6ef8d63788b17eb2d8da0f42ad2bc97c9a68fb8d
Kai Arnold <cdn.kai@gmail.com>
2018-07-17 12:21:22 -0700

    add the pretty player names to getGames

--- server/routes/api/getGames.js
+++ server/routes/api/getGames.js
@@ -4,6 +4,7 @@ const router = new express.Router();
 
 const mongoose = require('mongoose');
 const Game = mongoose.model('Game');
+const User = mongoose.model('User');
 
 router.post('/getGames', (req, res, next) => {
 
@@ -26,15 +27,40 @@ router.post('/getGames', (req, res, next) => {
    // {players: { $elemMatch: { user: ObjectId("5b10474ef20ae727305b8226") } } }
 
    logger.warn('testing getGames with a hard-coded ID');
-   Game.find().elemMatch('players', {'user': mongoose.Types.ObjectId('5b10474ef20ae727305b8226')}).exec((findErr, results) => {
+   Game.find().elemMatch('players', {'user': req.user._id}).exec((findErr, games) => {
 
       logger.info('elemMatch returns');
-      if (findErr || !results) {
+      if (findErr || !games) {
          logger.error('elemMatch error in getGames', findErr);
          return res.status(401).end();
       }
-      res.json({
-         games: results
+
+      // add in the player names for the client's convenience
+      const idsToNames = {};
+      let debug_playerCount = 0;
+      games.forEach((game) => {
+         game.players.forEach((player) => {
+            // just add an entry for now
+            idsToNames[player.user] = null;
+         });
+      });
+
+      const ids = Object.keys(idsToNames);
+
+      // look up the pretty names for the player ids and write them into the client's response
+      User.find({_id: { $in: ids }}, {name: 1}, (findIdsErr, users) => {
+         if (findIdsErr || !users || !users.length) {
+            logger.error('error finding pretty names', findIdsErr);
+            return res.status(401).end();
+         }
+
+         users.forEach((user) => {
+            idsToNames[user._id] = user.name;
+         });
+
+         console.log(idsToNames);
+
+         res.json({ games });
       });
    });
 });
