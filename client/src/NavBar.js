1d976e35d05f15aefa1c2142c813eb8dbcb53252
Kai Arnold <cdn.kai@gmail.com>
2018-09-09 15:25:04 -0700

    re-order navbar items

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,12 +6,13 @@ class NavBar extends Component {
       return (
          <nav className="sideNav lightUIPadding">
             <ul>
-               <li><Link to='/'>X</Link></li>
+               <li><Link to='/'>Color Match</Link></li>
                <li><Link to='/home/mygames'>My Games</Link></li>
-               <li><Link to='/home/leaderboard'>Leaderboard</Link></li>
                <li><Link to='/home/voting'>Voting</Link></li>
-               <li><Link to='/home/testboard'>Test Board</Link></li>
+               <li><Link to='/home/leaderboard'>Leaderboard</Link></li>
                <li><Link to='/welcome/logout'>Logout</Link></li>
+               <hr/>
+               <li><Link to='/home/testboard'>Test Board</Link></li>
             </ul>
          </nav>
       );
