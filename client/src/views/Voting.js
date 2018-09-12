6f543bb38b9bdfeff94f8703acfccb0e5d595880
Kai Arnold <cdn.kai@gmail.com>
2018-09-11 18:01:44 -0700

    fix small things, move closer to having voting done

--- client/src/views/Voting.js
+++ client/src/views/Voting.js
@@ -6,12 +6,17 @@ import GameBoard from './board/GameBoard';
 import rootStore from '../stores/rootStore';
 
 class Candidate extends Component {
+   constructor() {
+      super();
+      this.vote = this.vote.bind(this);
+   }
    vote() {
-
+      //rootStore.ballotStore.doVote();
+      console.log('would vote', this.props.index);
    }
    render() {
       return (
-         <button class='voteButton'>
+         <button className='voteButton' onClick={this.vote}>
             <GameBoard game={this.props.game} tileSize={60}/>
          </button>
       );
@@ -29,13 +34,13 @@ const VotingObserver = observer(class Voting extends Component {
       const ballot = rootStore.ballotStore.ballot;
       const renderedBallots = [];
       ballot.forEach(game => {
-         renderedBallots.push(<Candidate key={renderedBallots.length} game={game}/>);
+         renderedBallots.push(<Candidate key={renderedBallots.length} index={renderedBallots.length} game={game}/>);
       });
       return (
          <div>
             <h1>Voting</h1>
             <button onClick={this.test}>Refresh Ballot</button>
-            <div class='boardParent'>
+            <div className='boardParent'>
                {renderedBallots}
             </div>
          </div>
