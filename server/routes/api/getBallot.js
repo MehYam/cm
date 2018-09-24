b9e4b4f342b00d4256c29d1cce5407d620c12c07
Kai Arnold <cdn.kai@gmail.com>
2018-09-24 11:52:08 -0700

    use MongoDB aggregation to generate the ballot instead of our ad-hoc logic

--- server/routes/api/getBallot.js
+++ server/routes/api/getBallot.js
@@ -1,7 +1,5 @@
 const logger = require('../../logger');
 const express = require('express');
-const Chance = require('chance');
-const chance = new Chance();
 
 const router = new express.Router();
 
@@ -17,35 +15,25 @@ router.get('/getBallot', async (req, res, next) => {
 
    const user = req.user;
 
-   // get any existing unfinished ballot for this user
-   var ballot = user.currentBallot;
-
-   // if none exists, generate one and store it in the user doc
-   if (!ballot || !ballot.length) {
+   // create a ballot for this player unless they've got one saved
+   if (!user.currentBallot || !user.currentBallot.length) {
 
       logger.info('generating ballot for', user.name);
 
       // create a ballot from BALLOTSIZE completed games
       try {
-         const games = await Game.find({ completed: { $ne: null }})
-
-         const numGames = Math.min(games.length, BALLOTSIZE);
-         const randoms = chance.unique(chance.natural, numGames, {min: 0, max: games.length-1});
-         ballot = [];
-
          //KAI: need to scrub this data of user-specific info to ensure anonymity
-         randoms.forEach(r => { ballot.push(games[r]) });
+         const games = await Game.aggregate([ {$match: { completed: { $ne: null } } },  {$sample: {size: 3}} ]);
+         user.currentBallot = games;
 
-         user.currentBallot = ballot;
-
-         logger.info('currentBallot length:', ballot.length);
+         logger.info('currentBallot length:', user.currentBallot.length);
          await user.save();
       }
       catch (err) {
-         return res.status(500).send(err);
+         return res.status(400).send(err);
       }
    }
-   res.json({ballot});
+   res.json({ballot: user.currentBallot});
 });
 
 
