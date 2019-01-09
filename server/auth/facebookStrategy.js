3e3a975dddacf9af01986b04d45a1f031eab4194
Kai Arnold <cdn.kai@gmail.com>
2019-01-09 15:54:45 -0800

    add Jouko's mocks, make server work when 3rd party login config missing

--- server/auth/facebookStrategy.js
+++ server/auth/facebookStrategy.js
@@ -8,38 +8,43 @@ const config = require('../config');
 const fbConfig = { ...config.facebook };
 fbConfig.profileFields = [ 'id', 'displayName', 'name', 'picture' ];
 
-passport.use(new FacebookStrategy(
-   fbConfig,
-   async (accessToken, refreshToken, profile, done) => {
+if (fbConfig.clientID) {
+   logger.info('passport using FacebookStrategy');
+   passport.use(new FacebookStrategy(
+      fbConfig,
+      async (accessToken, refreshToken, profile, done) => {
 
-      logger.debug('FacebookStrategy, looking up', profile.displayName, profile.id);
+         logger.debug('FacebookStrategy, looking up', profile.displayName, profile.id);
 
-      const photoUrl = profile.photos && profile.photos.length ? profile.photos[0].value : null;
+         const photoUrl = profile.photos && profile.photos.length ? profile.photos[0].value : null;
 
-      // either find or create a user based on this Facebook profile
-      let user = null;
-      try {
-         user = await User.findOne({facebookId: profile.id});
-         if (!user) {
-            logger.debug('facebook id not found, creating new user');
+         // either find or create a user based on this Facebook profile
+         let user = null;
+         try {
+            user = await User.findOne({facebookId: profile.id});
+            if (!user) {
+               logger.debug('facebook id not found, creating new user');
 
-            const userData = {
-               facebookId: profile.id,
-               displayName: profile.displayName
-            };
-            user = new User(userData);
-            await user.save();
+               const userData = {
+                  facebookId: profile.id,
+                  displayName: profile.displayName
+               };
+               user = new User(userData);
+               await user.save();
+            }
+            else if (photoUrl && photoUrl != user.photoUrl) {
+               user.photoUrl = photoUrl;
+               await user.save();
+            }
          }
-         else if (photoUrl && photoUrl != user.photoUrl) {
-            user.photoUrl = photoUrl;
-            await user.save();
+         catch (err) {
+            logger.error('error in FacebookStrategy', err);
          }
+         done(null, user);
       }
-      catch (err) {
-         logger.error('error in FacebookStrategy', err);
-      }
-      done(null, user);
-   }
-));
+   ));
+}
+else {
+   logger.warn('no facebook clientID found, omitting FacebookStrategy');
 
-logger.info('passport using facebook strategy');
+}
--- server/auth/googleStrategy.js
+++ server/auth/googleStrategy.js
@@ -5,39 +5,44 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('mongoose').model('User');
 const config = require('../config');
 
-passport.use(new GoogleStrategy(
-   config.google,
-   async (req, accessToken, refreshToken, profile, done) => {
+if (config.google.clientID) {
+   logger.info('passport using GoogleStrategy');
 
-      logger.debug('GoogleStrategy, looking up', profile.displayName, profile.id);
+   passport.use(new GoogleStrategy(
+      config.google,
+      async (req, accessToken, refreshToken, profile, done) => {
 
-      // find or create a user based on this profile
-      // COPY/PASTA here with the facebook approach, not really worth consolidation though
-      const photoUrl = profile.photos && profile.photos.length ? profile.photos[0].value : null;
+         logger.debug('GoogleStrategy, looking up', profile.displayName, profile.id);
 
-      let user = null;
-      try {
-         user = await User.findOne({googleId: profile.id});
-         if (!user) {
-            logger.debug('google id not found, create new user');
+         // find or create a user based on this profile
+         // COPY/PASTA here with the facebook approach, not really worth consolidation though
+         const photoUrl = profile.photos && profile.photos.length ? profile.photos[0].value : null;
 
-            const userData = {
-               googleId: profile.id,
-               displayName: profile.displayName
-            };
-            user = new User(userData);
-            await user.save();
+         let user = null;
+         try {
+            user = await User.findOne({googleId: profile.id});
+            if (!user) {
+               logger.debug('google id not found, create new user');
+
+               const userData = {
+                  googleId: profile.id,
+                  displayName: profile.displayName
+               };
+               user = new User(userData);
+               await user.save();
+            }
+            else if (photoUrl && photoUrl != user.photoUrl) {
+               user.photoUrl = photoUrl;
+               await user.save();
+            }
          }
-         else if (photoUrl && photoUrl != user.photoUrl) {
-            user.photoUrl = photoUrl;
-            await user.save();
+         catch (err) {
+            logger.error('error in GoogleStrategy', err);
          }
+         done(null, user);
       }
-      catch (err) {
-         logger.error('error in GoogleStrategy', err);
-      }
-      done(null, user);
-   }
-));
-
-logger.info('passport using google strategy');
+   ));
+}
+else {
+   logger.warn('no google clientId found, omitting GoogleStrategy');
+}
